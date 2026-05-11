21a6b76a5315b69e3d06ff11797b6e1d479a295f
Mathew P <mathewsimonpanicker@gmail.com>
2026-05-11 14:27:55 -0500

    starting basic client app

--- client/app/main.py
+++ client/app/main.py
@@ -1,33 +1,38 @@
-import argparse
+import requests
 
-def connect(server):
-    print(f"Connecting to Weaver controller at {server}...")
+CONTROLLER_IP = "192.168.1.1"
+NAME = "MATHEWCLIENT1"
 
-def disconnect(server):
-    print(f"Disconnecting from Weaver controller at {server}...")
+def fetchClientPublicKey():
+    publicKey = "invalid"
+    with open('publickey', 'r') as f:
+        publicKey = f.readline().rstrip('\n')
+    return publicKey
 
-def status():
-    print("Weaver status...")
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog="weaver")
-    subparsers = parser.add_subparsers(dest="command")
+def connectToController():
+    API_KEY = input("enter API key - ")
+    
+    url = f"http://{CONTROLLER_IP}:5000/register"
+    headers = {"weaver-auth": API_KEY}
+    payload = {
+        "publicKey": fetchClientPublicKey(),
+        "name": NAME
+    }
+    
+    response = requests.post(url, json=payload, headers=headers)
+    if response.status_code != 200:
+        print(f"{response.json()['error']}.....exiting")
+        return -1
+    
+    controllerPublicKey = response.json()['controllerPublicKey']
+    allocatedIP = response.json()['allocatedIP']
 
-    connect_parser = subparsers.add_parser("connect")
-    connect_parser.add_argument("--server", required=True, help="Controller IP or hostname")
+    return 0
+    
+    
 
-    disconnect_parser = subparsers.add_parser("disconnect")
-    disconnect_parser.add_argument("--server", required=True, help="Controller IP or hostname")
 
-    subparsers.add_parser("status")
 
-    args = parser.parse_args()
-
-    if args.command == "connect":
-        connect(args.server)
-    elif args.command == "disconnect":
-        disconnect(args.server)
-    elif args.command == "status":
-        status()
-    else:
-        parser.print_help()
+if __name__ == '__main__':
+    connectToController()
--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apk add --no-cache wireguard-tools
+apk add --no-cache wireguard-tools py3-requests py3-pyroute2
 wg genkey | tee /app/privatekey | wg pubkey > /app/publickey
 echo "WireGuard keys generated."
 iperf3 -s -D
